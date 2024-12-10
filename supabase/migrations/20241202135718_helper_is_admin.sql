2e903ad172aa58092a4f84c0ef870ec5298d0b1a
Mike John Eviota <mike@codustry.com>
2024-12-10 19:37:17 +0800

    stupid typescript

--- supabase/migrations/20241202135718_helper_is_admin.sql
+++
@@ -1,5 +0,0 @@
-create or replace function is_admin() returns boolean as $$
-begin 
-    return exists(select 1 from roles_tb where role = 'admin' and user_id = auth.uid());
-end;
-$$ language plpgsql security definer;
--- supabase/migrations/20241202135724_helper_is_teacher.sql
+++
@@ -1,5 +0,0 @@
-create or replace function is_teacher() returns boolean as $$
-begin
-    return exists(select 1 from roles_tb where role = 'teacher' and user_id = auth.uid());
-end;
-$$ language plpgsql security definer;
