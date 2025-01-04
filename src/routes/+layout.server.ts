f3ef7a9fdabee8fa406d62461d455d2896900bbe
Mike John Eviota <eviotamikejohnb@gmail.com>
2025-01-04 16:30:19 +0800

    bump version

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -6,21 +6,20 @@ export const load: LayoutServerLoad = async ({
 }) => {
   const { session } = await safeGetSession();
 
-  /* const { data, error } = await supabaseAdmin.auth.admin.updateUserById(
-		'5eabd8a7-d32b-4acb-aa77-b28f5884a6a6',
-		{
-			password: 'admin123',
-			user_metadata: {
-				role: 'admin',
-				email: 'localadmin@gmail.com',
-				firstname: 'Charles',
-				middlename: '',
-				lastname: 'Ostria'
-			}
-		}
-	);
+  /*  const { data, error } = await supabaseAdmin.auth.admin.updateUserById(
+    'f357d399-2fa3-49ba-a909-6030fbf48144',
+    {
+      user_metadata: {
+        role: 'admin',
+        email: 'localadmin@gmail.com',
+        firstname: 'local',
+        middlename: '',
+        lastname: 'admin'
+      }
+    }
+  );
 
-	console.log(data, error?.message); */
+  console.log(data, error?.message); */
 
   return {
     session,
